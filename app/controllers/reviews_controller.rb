2eabcfa30e2f3dc66acaaef7c79ea108f172ada1
renaud rouas <renaud.rouas@gmail.com>
2018-05-09 16:23:23 +0200

    ca avnace

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+
+ def review
+    @review = @restaurant.review
+  end
+
+
+
+private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
